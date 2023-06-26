68dea134b46afe56d29e2654356f5bf84144fe63
ali herawi <aliherawi7@gmail.com>
2023-06-26 17:40:59 +0430

    new property added to the Relative entity and jpa realtions also implemented

--- src/main/java/com/mycompany/portalapi/models/Relative.java
+++ src/main/java/com/mycompany/portalapi/models/Relative.java
@@ -1,13 +1,10 @@
 package com.mycompany.portalapi.models;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.mycompany.portalapi.config.RelationDeserializer;
-import com.mycompany.portalapi.constants.RelationName;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.*;
 
 
 @Entity

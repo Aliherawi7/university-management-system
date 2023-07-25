a3e9566fe6bbed6c2efcf84ecf4ff249cd10d788
ali herawi <aliherawi7@gmail.com>
2023-07-25 08:18:37 +0430

    lock and unlock response message updated

--- src/main/java/com/mycompany/portalapi/controllers/AuthController.java
+++ src/main/java/com/mycompany/portalapi/controllers/AuthController.java
@@ -1,13 +1,18 @@
 package com.mycompany.portalapi.controllers;
 
+import com.mycompany.portalapi.dtos.APIResponse;
 import com.mycompany.portalapi.dtos.AuthenticationRequest;
 import com.mycompany.portalapi.dtos.LoginResponse;
 import com.mycompany.portalapi.dtos.UpdateUserDTO;
 import com.mycompany.portalapi.services.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -24,12 +29,26 @@ public class AuthController {
     @PutMapping("lock/{userId}")
     public ResponseEntity<?> lockUserById(@PathVariable Long userId) {
         authenticationService.lockUserById(userId);
-        return ResponseEntity.ok("user account with id: {" + userId + "} has been locked successfully");
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .httpStatus(HttpStatus.OK)
+                        .message("حساب کاربری مورد نظر با موفقیت غیر فعال شد!")
+                        .zonedDateTime(ZonedDateTime.now(ZoneId.of("UTC")))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
     }
     @PutMapping("unLock/{userId}")
     public ResponseEntity<?> unLockUserById(@PathVariable Long userId) {
         authenticationService.unLockUserById(userId);
-        return ResponseEntity.ok("user account with id: {" + userId + "}has been unlocked successfully");
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .httpStatus(HttpStatus.OK)
+                        .message("حساب کاربری مورد نظر با موفقیت فعال شد!")
+                        .zonedDateTime(ZonedDateTime.now(ZoneId.of("UTC")))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+                );
     }
 
     @PutMapping("/update-user")
